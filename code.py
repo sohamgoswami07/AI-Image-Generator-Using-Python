bb7138cd6551edcec4be416981519a8f36f53bfe
Soham Goswami <sohamgoswami07@gmail.com>
2023-11-26 01:54:50 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,15 @@
 import os
-import openai  # pip install openai
+import openai   # pip install openai
 import urllib.request
 from datetime import datetime
 
-openai.api_key = "API_KEY" # Replace API_KEY with your own valid API Key 
+openai.api_key = "API_KEY"   # Replace "API_KEY" with your own valid API Key 
 
 user_prompt = input("Write your prompt for DALL-E 2: ")
 
 response = openai.Image.create(
     prompt = user_prompt,
-    n = 1,
+    n = 1,   # by increasing the value of "n" you can get more results (max value of "n" is 10)
     size = "1024x1024"
 )
 
